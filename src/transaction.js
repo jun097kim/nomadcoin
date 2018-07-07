cfbe9da729c85f678bf394b035c740b0c3a4e2a4
jun097kim <jun097kim@gmail.com>
2018-07-07 23:50:25 +0900

    #34 Getting the Transaction ID

--- src/transaction.js
+++ src/transaction.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class TxOut {
   constructor(address, amount) {
     this.address = address;
@@ -12,6 +14,30 @@ class TxIn {
 }
 
 class Transaction {
-  
+  // ID
+  // txIns[]
+  // txOuts[]
 }
-]
+
+class UTxOut {
+  constructor(uTxOutId, uTxOutIndex, address, amount) {
+    this.uTxOutId = uTxOutId;
+    this.uTxOutIndex = uTxOutIndex;
+    this.address = address;
+    this.amount = amount;
+  }
+}
+
+let uTxOuts = [];
+
+const getTxId = tx => {
+  const txInsContent = tx.txIns
+    .map(txIn => txIn.uTxOutId + txIn.UTxOut)
+    .reduce((a, b) => a + b, "");
+
+  const txOutsContent = tx.txOuts
+    .map(txOut => txOut.address + txOut.amount)
+    .reduce((a, b) => a + b, "");
+
+  return CryptoJS.SHA256(txInsContent + txOutsContent).toString();
+};
