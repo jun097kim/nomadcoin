36d910c2e5733ea6d5959a9eef34dcda46e1ee47
jun097kim <jun097kim@gmail.com>
2018-07-08 11:18:32 +0900

    #50 Creating the Wallet

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   morgan = require("morgan"),
   Blockchain = require("./blockchain"),
-  P2P = require("./p2p");
+  P2P = require("./p2p"),
+  Wallet = require("./wallet");
 
 const { getBlockChain, createNewBlock } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
+const { initWallet } = Wallet;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -33,4 +35,5 @@ const server = app.listen(PORT, () =>
   console.log(`Nomadcoin Server HTTP Server running on port ${PORT} ✅`)
 );
 
+initWallet();
 startP2PServer(server);
