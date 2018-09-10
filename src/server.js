709a079f8841879c29c7bce2b4a6ff0b77e527e4
jun097kim <jun097kim@gmail.com>
2018-09-11 02:07:55 +0900

    #75 내 주소를 가져오는 API 추가

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const express = require('express'),
 
 const { getBlockChain, createNewBlock, getAccountBalance, sendTx } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
-const { initWallet } = Wallet;
+const { initWallet, getPublicFromWallet } = Wallet;
 const { getMempool } = Mempool;
 
 const PORT = process.env.HTTP_PORT || 3000;
@@ -35,6 +35,10 @@ app.post('/peers', (req, res) => {
 app.get('/me/balance', (req, res) => {
   const balance = getAccountBalance();
   res.send({ balance });
+});
+
+app.get('/me/address', (req, res) => {
+  res.send(getPublicFromWallet());
 });
 
 app
